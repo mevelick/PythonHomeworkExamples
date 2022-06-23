f1351411d26d51a6d6054ae1c700557d0cf3fd6e
Marni <mevelick@gmail.com>
2022-06-23 14:45:03 -0400

    Create class_Car.py
    This program creates a class Car and creates a Car object

---
+++ class_Car.py
@@ -0,0 +1,35 @@
+# Defining Car class
+class Car:
+
+    # Initialize class attributes of the class
+    def __init__(self, year_model, make):
+        self.__year_model = year_model
+        self.__make = make
+        self.__speed = 0
+
+    # Defining class methods
+    def accelerate(self):
+        self.__speed += 5
+
+    def brake(self):
+        self.__speed -= 5
+
+    def get_speed(self):
+        return self.__speed
+
+# Defining the main function
+def main():
+    # Creating a "Car" object
+    my_car = Car("2022 Trailblazer", "Chevy")
+
+    # Using "for" loops to run the methods 5x
+    for i in range(5):
+        my_car.accelerate()
+        print(my_car.get_speed())
+
+    for i in range(5):
+        my_car.brake()
+        print(my_car.get_speed())
+
+# Calling the main function to execute the code
+main()
