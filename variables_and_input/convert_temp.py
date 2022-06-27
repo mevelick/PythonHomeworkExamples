e70dd750863803b349c6c2f66f2a03d16a282e76
Marni <mevelick@gmail.com>
2022-06-26 21:28:11 -0400

    Create convert_temp.py

---
+++ variables_and_input/convert_temp.py
@@ -0,0 +1,7 @@
+# Asking the user to input a temperature in Celsius and initializing the variable
+temp_c = float(input("Please enter a temperature in degrees Celsius: "))
+
+# Formula to convert Celsius to Fahrenheit
+temp_f = (temp_c * 1.8) + 32
+
+print(temp_f)
