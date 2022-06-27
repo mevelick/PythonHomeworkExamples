96da507100b96e66e2ec4c9734679bfeedb97807
Marni <mevelick@gmail.com>
2022-06-26 21:26:37 -0400

    Create greet.py

---
+++ variables_and_input/greet.py
@@ -0,0 +1,7 @@
+
+# Asking the user to input their name & initializing the variables
+first_name = input("Please enter your first name: ")
+last_name = input("Please enter your last name: ")
+
+# Welcome message that will output to the terminal
+print("Welcome", first_name,last_name)
